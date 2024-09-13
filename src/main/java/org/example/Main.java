a9e9c09c520bca0d0092ac5638a7b8bf32287e0b
carlosdevnt <carlosdev54@gmail.com>
2024-09-13 15:17:49 -0400

    Calculator in Java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,25 @@
 package org.example;
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+/*
+ * ============================================
+ *               CALCULADORA BÁSICA
+ * ============================================
+ * Esta aplicação realiza as quatro operações
+ * básicas (soma, subtração, multiplicação,
+ * divisão) e módulo.
+ *
+ * Autor: Carlos
+ * Data: 13/09/2024
+ * Versão: 1.0.1
+ * ============================================
+ */
+
+
 public class Main {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
+    System.out.println("Seja muito bem-vindo!");
     System.out.printf("Digite o primeiro número:\n");
     double firstNumber = scanner.nextDouble();
     System.out.printf("Digite o segundo número:\n");
@@ -21,25 +35,34 @@ public class Main {
     switch (operation) {
         case 1:
             result = firstNumber + secondNumber;
+            System.out.printf("A soma entre %.1f e %.1f resulta em %.2f\n", firstNumber, secondNumber, result);
                 break;
         case 2:
             result = firstNumber - secondNumber;
+            System.out.printf("A subtração entre %.1f e %.1f resulta em %.2f\n", firstNumber, secondNumber, result);
                 break;
         case 3:
             result = firstNumber * secondNumber;
+            System.out.printf("A multiplicação entre %.1f e %.1f resulta em %.2f\n", firstNumber, secondNumber, result);
             break;
         case 4:
             if (secondNumber != 0){
                 result = firstNumber / secondNumber;
+                System.out.printf("A divisão entre %.1f e %.1f resulta em %.2f\n", firstNumber, secondNumber, result);
             } else {
-                System.out.println("Erro: Divisão por zero.");
+                System.out.println("Erro: Divisão por zero.\n");
                 return;
             }
             break;
         case 5:
             result = firstNumber % secondNumber;
+            System.out.printf("O resto da divisão entre %.1f e %.1f resulta em %.2f\n", firstNumber, secondNumber, result);
             break;
+        default:
+            System.out.println("Operação inválida.\n");
+            return;
+
     }
-        System.out.println(result);
+
     }
 }
